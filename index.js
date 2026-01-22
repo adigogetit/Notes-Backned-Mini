2693c337b44a8e2b5c643aea4be39c00b6645e84
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-01-23 00:13:23 +0530

    notes-backned

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('node:path');
+const app = express();
+const fs = require("fs"); 
+
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  fs.readdir(`./files`, (error, files) => {
+    res.render("index", { files: files });
+  })
+});
+
+app.get('/file/:filename', (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show",{filename: req.params.filename.replace(".txt", "") , filedata: filedata})
+  })
+});
+
+app.post('/create', (req, res) => {
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {
+    res.redirect("/")
+  })
+})
+
+app.listen(3000);
